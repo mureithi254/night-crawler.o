bc85396dc9ae099c82d341c4b9ce54288fc7a73f
mureithi254 <lnewhenio@gmail.com>
2017-02-25 17:29:42 +0300

    Finishing-comparisons

--- Night-crawler.rb
+++ Night-crawler.rb
@@ -2,13 +2,13 @@ class Anemon
    require 'anemone'
    require 'nokogiri'
    require 'open-uri'
-   require 'classifier-reborn'
+   # require 'classifier-reborn'
 
 
    def initialize(url)
      @url = url 
    end
-
+  
    def crawler
      Anemone.crawl(@url) do |anemone|
        anemone.on_every_page do |page|
@@ -19,10 +19,10 @@ class Anemon
   end
 
   def scrapper
-     @urls.each do| urli|
+     @urls.each do |urli|
         html_data = open(urli).read
         nokogiri_object = Nokogiri::HTML(html_data)
-        elements = nokogiri_object.xpath("//a")
+        elements = nokogiri_object.xpath("//p")
 
         elements.each do |element|
           @data = element.text
@@ -30,24 +30,55 @@ class Anemon
     end
   end
 
-  def docs(file_name)
-    @docs = File.readlines(file_name)
+  def new_file 
+    #File.file?(filename) will only return true for files
+     #File.exists?(filename) will only return true for directories - watch out
+    if File.file?("scrapped.txt")
+        File.open("scrapped.txt","w"){ |file| file.write(@data)}
+    else
+       scrapperFile  = File.new("scrapped.txt","w+")
+        File.open("scrapped.txt","w"){ |file| file.write(@data)}
+       scrapperFile.close
+    end
+    @scrap_file = File.readlines("scrapped.txt")
+  end
+
+  def uploaded_docs(file_name)
+    @upload_docs = File.readlines(file_name)
   end
   
-  def comparative
-    lsi = ClassifierReborn::LSI.new
-    lsi.add_item @data
+  # def comparative
+  #   lsi = ClassifierReborn::LSI.new
 
-    @docs.each do |line|
-      lsi.classify line
-      puts lsi.find_related(line)
-    end 
+  #   @scrap_file.each do |scrap_line|
+  #      lsi.add_item scrap_line  
+  #   end
+
+  #   @upload_docs.each do |upload_line|
+  #     lsi.classify upload_line
+  #     lsi.find_related(uploaded_line)
+
+  #   end 
+  # end
+
+  def comparative(file_name)
+    @upload_lines = File.readlines(file_name)
+
+      @upload_docs.each do |upload_doc_line|
+        if @upload_lines.include?(upload_doc_line)
+           puts "Instances of plagiarism were found on the following lines: "
+           puts "#{upload_doc_line}"
+         else
+          puts "No plagiarism was found in the documents"
+        end
+      end 
   end
 
 end
 
-google = Anemon.new('http://www.habahaba.co/')
-google.crawler
-google.scrapper
-google.docs("makefile.txt")
-google.comparative
+plag = Anemon.new('http://www.habahaba.co/')
+plag.crawler
+plag.scrapper
+plag.new_file
+plag.uploaded_docs("makefile.txt")
+plag.comparative("scrapped.txt")
